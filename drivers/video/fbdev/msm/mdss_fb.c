ccc20342dd02ced52777a2b4720138a63e4cf99a
Sultan Alsawaf <sultan@kerneltoast.com>
2019-04-17 18:02:18 -0700

    msm: mdss: Boost cpubw devfreq device when a new frame is ready
    The cpubw devfreq device delegates the speed of the DDR bus, so its
    speed is directly correlated with frame drops. Boost it when a new frame
    is ready to be rendered to the display to reduce overall frame drops.
    
    Signed-off-by: Sultan Alsawaf <sultanxda@gmail.com>

--- drivers/video/fbdev/msm/mdss_fb.c
+++ drivers/video/fbdev/msm/mdss_fb.c
@@ -46,6 +46,7 @@
 #include "mdss_smmu.h"
 #include "mdss_mdp.h"
 #include "mdss_sync.h"
+ #include <linux/devfreq_boost.h>
 
 #ifdef CONFIG_FB_MSM_TRIPLE_BUFFER
 #define MDSS_FB_NUM 3
@@ -5052,6 +5053,7 @@ int mdss_fb_do_ioctl(struct fb_info *info, unsigned int cmd,
 		ret = mdss_fb_mode_switch(mfd, dsi_mode);
 		break;
 	case MSMFB_ATOMIC_COMMIT:
+	        devfreq_boost_kick(DEVFREQ_MSM_CPUBW);
 		ret = mdss_fb_atomic_commit_ioctl(info, argp, file);
 		break;
 
