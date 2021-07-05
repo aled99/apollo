54364cdf6d2705193aa5121e676e630a87e5bcf6
Sultan Alsawaf <sultan@kerneltoast.com>
2021-07-04 22:35:09 -0400

    [FIXUP] c4c999c ("devfreq_boost: Introduce devfreq boost driver")
    This driver boosts enumerated devfreq devices upon input, and allows for
    boosting specific devfreq devices on other custom events. The boost
    frequencies for this driver should be set so that frame drops are
    near-zero at the boosted frequencies and power consumption is minimized
    at said frequencies. The goal of this driver is to provide an interface
    to achieve optimal device performance by requesting boosts on key
    events, such as when a frame is ready to rendered to the display.
    
    Currently, support is only present for boosting the cpubw devfreq
    device, but the driver is structured in a way that makes it easy to add
    support for new boostable devfreq devices in the future.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    [@0ctobot: Bring inline with kerneltoast/android_kernel_google_floral@dce5aa5,
    amending the following commits where applicable:
    076de0a ("[2/2] devfreq_boost: Rework display state awareness logic for OnePlus 8 devices")
    bf07438 ("devfreq_boost: Update and expand to handle CPUBW/LLCCBW boosting")
    2ca6410 ("devfreq_boost: Mark boost kthreads as performance critical")]
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

--- drivers/gpu/drm/drm_atomic.c
+++ drivers/gpu/drm/drm_atomic.c
@@ -2576,6 +2576,13 @@ int drm_mode_atomic_ioctl(struct drm_device *dev,
 			(arg->flags & DRM_MODE_PAGE_FLIP_EVENT))
 		return -EINVAL;
 
+	/* Boost CPU and DDR when committing a new frame */
+	if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY)) {
+		cpu_input_boost_kick();
+		devfreq_boost_kick(DEVFREQ_CPU_LLCC_DDR_BW);
+		devfreq_boost_kick(DEVFREQ_CPU_CPU_LLCC_BW);
+	}
+
 	drm_modeset_acquire_init(&ctx, DRM_MODESET_ACQUIRE_INTERRUPTIBLE);
 
 	state = drm_atomic_state_alloc(dev);
