ced48f4463b429fa864369ea8726dd17a2627c5f
aled99 <bumbi@live.it>
2021-08-23 20:19:29 +0200

    kernel: Affine hwcomposer to prime CPUs
    HWC in Android is responsible for communicating userspace's graphics
    requests with the kernel. Affining it to fast CPUs greatly reduces
    jitter.

--- fs/exec.c
+++ fs/exec.c
@@ -1849,7 +1849,7 @@ static int __do_execve_file(int fd, struct filename *filename,
 					   HWCOMPOSER_BIN_PREFIX,
 					   strlen(HWCOMPOSER_BIN_PREFIX)))) {
 			current->flags |= PF_PERF_CRITICAL;
-			set_cpus_allowed_ptr(current, cpu_perf_mask);
+			set_cpus_allowed_ptr(current, cpu_prime_mask);
 		}
 	}
 
